6d90fefb42e6739e63ab91ca73c99726278b736d
FrancisMurayaDev <francismurayadev@gmail.com>
2025-04-07 09:59:49 +0300

    remove div text

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import RegistrationForm from "./Components/RegistrationForm/RegistrationForm";
 function App() {
   return (
     <div>
-      MUI Registration Form
       <RegistrationForm />
     </div>
   );
